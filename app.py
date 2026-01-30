bf4ac745cebd76fecd8495de75f7b01f806a612a
Julio Antloga <julio.antloga@mindsight.com.br>
2026-01-30 18:45:12 -0300

    upload 6

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
 import time
 import os
 from traceback import format_exc
